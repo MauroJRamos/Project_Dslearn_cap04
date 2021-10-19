0d03f34c1ee847dc950104af93e04a3a8dd38f13
MauroJRamos <mauroramosti@gmail.com>
2021-10-19 11:18:37 -0300

    Enrollment seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -25,3 +25,6 @@ INSERT INTO tb_resource (title, description, position, img_Uri, type, offer_id) 
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 01', 'Neste capítulo vamos começar', 1, 'https://www.publicdomainpictures.net/pictures/280000/velka/family-relationship-course.jpg', 1, null);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 02', 'Neste capítulo vamos continuar', 2, 'https://www.publicdomainpictures.net/pictures/280000/velka/family-relationship-course.jpg', 1, 1);
 INSERT INTO tb_section (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capitulo 03', 'Neste capítulo vamos finlizar', 3, 'https://www.publicdomainpictures.net/pictures/280000/velka/family-relationship-course.jpg', 1, 2);
+
+INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, avaliable, only_Update) VALUES (1, 1,TIMESTAMP WITH TIME ZONE '2021-10-18T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment(user_id, offer_id, enroll_Moment, refund_Moment, avaliable, only_Update) VALUES (2, 1,TIMESTAMP WITH TIME ZONE '2021-10-18T13:00:00Z', null, true, false);
